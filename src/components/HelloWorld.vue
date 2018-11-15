1cd113987430a4b4cd01c14741d17c77f680de6b
jacques <jacques.sounvi@gmail.com>
2018-11-15 16:10:43 +0100

    Action to start game

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,51 @@
 <template>
  <div>
-   <ul>
-     <li v-for="(ingredient,i) in ingredients">{{ingredient}} {{i}}</li>
-   </ul>
-   <button @click="ingredients.push('spices')">Add new</button>
-   <hr>
+   <section class="row">
+        <div class="small-6 columns">
+            <h1 class="text-center">YOU</h1>
+            <div class="healthbar">
+                <div 
+                  class="healthbar text-center" 
+                  style="background-color: green; margin: 0; color: white;"
+                  :style="{width: playerHealth + '%'}">
+                    {{playerHealth}}
+                </div>
+            </div>
+        </div>
+        <div class="small-6 columns">
+            <h1 class="text-center">MONSTER</h1>
+            <div class="healthbar">
+                <div 
+                  class="healthbar text-center" 
+                  style="background-color: green; margin: 0; color: white;"
+                  :style="{width: monsterHealth + '%'}">
+                    {{monsterHealth}}
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="row controls" v-if="!gameIsRunning">
+        <div class="small-12 columns">
+            <button @click="startgame" id="start-game">START NEW GAME</button>
+        </div>
+    </section>
+    <section class="row controls" v-else>
+        <div class="small-12 columns">
+            <button id="attack">ATTACK</button>
+            <button id="special-attack">SPECIAL ATTACK</button>
+            <button id="heal">HEAL</button>
+            <button id="give-up">GIVE UP</button>
+        </div>
+    </section>
+    <section class="row log">
+        <div class="small-12 columns">
+            <ul>
+                <li>
 
-   <ul>
-     <li v-for="person in persons">
-       <div v-for="(value,key) in person">
-         {{key}} : {{value}} 
-       </div>
-      </li>
-   </ul>
+                </li>
+            </ul>
+        </div>
+    </section>
  </div>
   
 
@@ -23,39 +56,116 @@
 export default {
   data() {
     return{
-
-      ingredients: ['meat','fruit','cookies'],
-      persons:[
-        {name: 'max',age: 27, color:'red'},
-        {name: 'jack',age: 28, color:'blue'},
-      ]
+      playerHealth: 100,
+      monsterHealth: 100,
+      gameIsRunning: false 
     };
-    
-  }
+  },
+  methods: {
+      startgame() {
+        this.gameIsRunning = true;
+      }
+
+    }
+  
+     
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.text-center {
+    text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.healthbar {
+    width: 80%;
+    height: 40px;
+    background-color: #eee;
+    margin: auto;
+    transition: width 500ms;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.controls, .log {
+    margin-top: 30px;
+    text-align: center;
+    padding: 10px;
+    border: 1px solid #ccc;
+    box-shadow: 0px 3px 6px #ccc;
 }
-a {
-  color: #42b983;
+
+.turn {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-weight: bold;
+    font-size: 22px;
 }
-.demo{
-  width: 100px;
-  height: 100px;
-  background-color: gray;
-  display: inline-block;
-  margin: 10px;
+
+.log ul {
+    list-style: none;
+    font-weight: bold;
+    text-transform: uppercase;
+}
+
+.log ul li {
+    margin: 5px;
+}
+
+.log ul .player-turn {
+    color: blue;
+    background-color: #e4e8ff;
+}
+
+.log ul .monster-turn {
+    color: red;
+    background-color: #ffc0c1;
+}
+
+button {
+    font-size: 20px;
+    background-color: #eee;
+    padding: 12px;
+    box-shadow: 0 1px 1px black;
+    margin: 10px;
+}
+
+#start-game {
+    background-color: #aaffb0;
+}
+
+#start-game:hover {
+    background-color: #76ff7e;
+}
+
+#attack {
+    background-color: #ff7367;
+}
+
+#attack:hover {
+    background-color: #ff3f43;
+}
+
+#special-attack {
+    background-color: #ffaf4f;
+}
+
+#special-attack:hover {
+    background-color: #ff9a2b;
+}
+
+#heal {
+    background-color: #aaffb0;
+}
+
+#heal:hover {
+    background-color: #76ff7e;
+}
+
+#give-up {
+    background-color: #ffffff;
+}
+
+#give-up:hover {
+    background-color: #c7c7c7;
 }
 </style>
