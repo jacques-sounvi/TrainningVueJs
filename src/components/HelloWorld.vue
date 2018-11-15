840ca881cc8c7d367afcd6e70732ab7513730ccd
jacques <jacques.sounvi@gmail.com>
2018-11-15 11:27:43 +0100

    Dynamic style css in vue js

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+ <div>
+   <div class="demo" :style="{backgroundColor: color}"></div>
+   <div class="demo"></div>
+   <div class="demo"></div>
+   <hr>
+
+   <input type="text" v-model="color">
+
+ </div>
+  
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      color: 'green'
+    };
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.demo{
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  display: inline-block;
+  margin: 10px;
+}
+</style>
