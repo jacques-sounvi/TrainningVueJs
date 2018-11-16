274796ed6bda9f99807e8f31b4d8e998d559db41
jacques <jacques.sounvi@gmail.com>
2018-11-16 15:31:33 +0100

    Add element to component Quote

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote></app-quote>
+                <app-quote>
+                    <h2>the quote</h2>
+                    <p>A wonder Quote</p>
+                </app-quote>
                
             </div>
         </div>
