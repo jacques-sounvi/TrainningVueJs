3f668a00d9388eb55a98047a6066b0458f606114
jacques <jacques.sounvi@gmail.com>
2018-11-16 13:16:07 +0100

    Use event to communicate with parent

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="component">
         <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
+        <p>I'm an awesome {{name}}</p>
+        <button @click="ChangeName">Change name</button>
+        <p>{{name}}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -22,7 +24,18 @@
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
-        }
+        },
+         data(){
+           return{
+               name: 'max'
+           };
+       },
+       methods:{
+           ChangeName(){
+               this.name = 'Anna'
+           }
+
+       }
     }
 </script>
 
