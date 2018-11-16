424b1b5a6e4d152b6871f469b23cbeb5ed82006a
jacques <jacques.sounvi@gmail.com>
2018-11-16 13:17:25 +0100

    Use  to define a directive v-on

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,25 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name : {{swithName()}} </p>
+         <button @click="ResetName">Reset name</button>
+
     </div>
 </template>
 
 <script>
+    export default{
+        props: ['myName'],
+        methods:{
+            swithName(){
+                return this.myName.split("").reverse().join("");
+            },
+            ResetName(){
+                this.myName = 'Max'
+                 this.$emit('nameWasReset',this.myName);
+            }
+        }
+    }
 </script>
 
 <style scoped>
